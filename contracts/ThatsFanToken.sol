d5e38dee411aacd69582c81d2ef2cfe0381d76a7
Domenico Barra <domenico@adamantic.io>
2023-08-01 17:52:32 +0200

    (fix) Added storage gap for future upgrade Addresses issue CERTIK-TFT-09

--- contracts/ThatsFanToken.sol
+++ contracts/ThatsFanToken.sol
@@ -127,4 +127,11 @@ contract ThatsFanToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradea
     {
         super._beforeTokenTransfer(from, to, amount);
     }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[49] private __gap;
 }
